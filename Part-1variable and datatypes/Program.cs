db0f0139a9d20472f0421bd3e3cce6c13df93800
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-09 15:08:31 +0530

    Part-1variable and datatypes

---
+++ Part-1variable and datatypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1variable_and_datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1: Variables and Data Types
+            string name = "Hema";
+            int age = 25; // Replace with your actual age
+            double height = 175.5; // Replace with your actual height
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Height: {height} cm");
+            Console.ReadKey();
+        }
+    }
+}
